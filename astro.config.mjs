8c0b4b2e8410950e04ee061200f4da3d8bf48b34
Esteban <juanesbs2003@hotmail.com>
2023-09-12 15:23:29 -0500

    optimización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), compress({ 
+    Path: ["./Target", "./Build"],
+    CSS: true,
+    HTML: true,
+    Image: false,
+    JavaScript: true,
+    SVG: false,
+  })]
 });
